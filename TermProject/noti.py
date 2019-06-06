66b75326df2ce0e2e1fbf98ef77b8ebae366b971
minsuSong <minsu7917@gmail.com>
2019-06-06 17:43:36 +0900

    simple telegram

---
+++ TermProject/noti.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python
+# coding=utf-8
+
+import sys
+import time
+import sqlite3
+import telepot
+from pprint import pprint
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import re
+from datetime import date, datetime, timedelta
+import traceback
+#################
+import LoLTKinter
+import LOL_ParseJson as LP
+#################
+key = 'RGAPI-354e7489-f932-4a39-90ab-24069b93c837'
+server = "kr.api.riotgames.com"
+TOKEN = '679122916:AAEv-dCunBmBa3IS96ZJrK087PG__FuuzKA'
+MAX_MSG_LENGTH = 300
+bot = telepot.Bot(TOKEN)
+
+parser = LP.Parser(server, key)
+
+def getData(in_summonerName):
+    res_list = []
+    global parser
+
+    res_list = parser.Get_API_Search_byName(in_summonerName)
+
+    return res_list
+
+def sendMessage(user, msg):
+    try:
+        bot.sendMessage(user, msg)
+    except:
+        traceback.print_exc(file=sys.stdout)
+
+def run(in_summonerName):
+    conn = sqlite3.connect('logs.db')
+    cursor = conn.cursor()
+    cursor.execute('CREATE TABLE IF NOT EXISTS logs( user TEXT, log TEXT, PRIMARY KEY(user, log) )')
+    conn.commit()
+
+    user_cursor = sqlite3.connect('users.db').cursor()
+    user_cursor.execute('CREATE TABLE IF NOT EXISTS users( user TEXT, location TEXT, PRIMARY KEY(user, location) )')
+    user_cursor.execute('SELECT * from users')
+
+    for data in user_cursor.fetchall():
+        user, param = data[0], data[1]
+        print(user, param)
+        res_list = getData( in_summonerName)
+        msg = ''
+        for r in res_list:
+            try:
+                cursor.execute('INSERT INTO logs (user,log) VALUES ("%s", "%s")'%(user,r))
+            except sqlite3.IntegrityError:
+                # 이미 해당 데이터가 있다는 것을 의미합니다.
+                pass
+            else:
+                print( str(datetime.now()).split('.')[0], r )
+                if len(r+msg)+1>MAX_MSG_LENGTH:
+                    sendMessage( user, msg )
+                    msg = r+'\n'
+                else:
+                    msg += r+'\n'
+        if msg:
+            sendMessage( user, msg )
+    conn.commit()
+
+if __name__=='__main__':
+    today = date.today()
+    current_month = today.strftime('%Y%m')
+
+    print( '[',today,']received token :', TOKEN )
+
+    pprint( bot.getMe() )
+
+    run(current_month)
